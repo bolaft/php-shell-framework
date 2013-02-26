8487aebc56b7ac6bafaf4af7f71067fdfa4cd77d
Soufian <soufian.salim@gmail.com>
2013-02-26 17:15:25 +0100

    added a license paragraph

--- tests/integration/ncurses.php
+++ tests/integration/ncurses.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CursedScript package.
+ *
+ * (c) Soufian Salim <soufi@nsal.im>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 // we begin by initializing ncurses
 
 $ncurse = ncurses_init();
